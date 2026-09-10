3598d8bb525b550b00a4f5e7bb4b071d0d21c5fe
xmqian03 <2537831239@qq.com>
2026-09-10 03:52:09 +0000

    Deploying to gh-pages from @ xmqian03/xmqian03.github.io@9587bac93a497d6b5fa49fb361352787fc4bce69 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,9 +30,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-简历",
+          title: "简历",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
